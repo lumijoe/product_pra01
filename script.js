73139e665428ea85b4dc8f47698876f721a6c315
lumijoe <spacedesign63@gmail.com>
2024-05-26 23:21:21 +0900

    test

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // .telnum=電話番号
-const telNum = "177";
-document.querySelectorAll(".telnum").forEach(
+const telNum = "03-xxxx-xxx3";
+document.querySelectorAll(".telNum").forEach(
     function(element) {
         element.innerHTML="<a href='tel:" + telNum + "'>" + telNum + "</a>";
     }
@@ -8,8 +8,30 @@ document.querySelectorAll(".telnum").forEach(
 
 // .telinfo=電話情報
 const telInfo = "受付時間 10:00~18:00 【土日祝定休日】";
-document.querySelectorAll(".telinfo").forEach(
+document.querySelectorAll(".telInfo").forEach(
     function(element) {
         element.innerHTML=telInfo;
     }
 );
+
+document.addEventListener("DOMContentLoaded", function() {
+    // テキストのリスト
+    const newTexts = [
+        "選ばれる理由",
+        "サービス紹介",
+        "導入までの流れ",
+        "料金プラン",
+        "よくあるご質問"
+    ];
+
+    // nav-item クラスを持つ全てのリスト項目を取得
+    const navItems = document.querySelectorAll(".nav-item");
+
+    // navItems をループしてテキストを変更
+    navItems.forEach((item, index) => {
+        // それぞれのリスト項目のテキストを新しいテキストに置き換える
+        if (newTexts[index]) {
+            item.querySelector("a").textContent = newTexts[index];
+        }
+    });
+});
